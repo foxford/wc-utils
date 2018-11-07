87037a5e646fa6ebb46a01fe1e759a917d6a5dab
Sergo <rogaldh@radsh.red>
2018-11-07 17:00:38 +0300

    Improve bundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,21 @@
 import { terser as uglify } from 'rollup-plugin-terser'
+import babel from 'rollup-plugin-babel'
 
 const entry = process.env.ENTRY || 'index.js'
 const ns = process.env.NS || 'WCMixins'
 
 const uglifyOptions = {
   compress: {
+    drop_console: true,
     pure_getters: true,
-    unsafe: true,
     unsafe_comps: true,
     warnings: false,
   },
 }
 
-const shouldUglify = (options = uglifyOptions, minifier) => process.env.NODE_ENV === 'production' ? uglify(options, minifier) : []
+const shouldUglify = (options = uglifyOptions, minifier) => process.env.NODE_ENV === 'production'
+  ? uglify(options, minifier)
+  : []
 
 const es = it => ({
   input: `src/${it}`,
@@ -21,7 +24,7 @@ const es = it => ({
     format: 'umd',
     name: ns,
   },
-  plugins: shouldUglify(uglifyOptions),
+  plugins: [babel()].concat(shouldUglify(uglifyOptions)),
 })
 
 export default es(entry)
